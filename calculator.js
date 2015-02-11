c78049b79b0f7e168154a7b90b8a7ca5cb33dd64
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-10 17:30:23 -0700

    jQuery transition on button press

--- calculator.js
+++ calculator.js
@@ -1,11 +1,21 @@
 var Calculator = {};
 
+var pushButton = function() {
+  $('.key').mousedown(function() {
+    $(this).addClass("pressed").mouseup(function() {
+      $(this).removeClass("pressed");
+    });
+  });
+};
+
 Calculator.init = function() {
   //add
   //subtract
   //multiply
   //divide
+  pushButton();
 };
+
 
 $(document).ready(function() {
   Calculator.init();
